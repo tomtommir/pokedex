45b351ef5ea167a0077613fb73e015fe9dafab8d
tom <tomtommir@hotmail.fr>
2022-04-10 20:38:46 +0200

    listing pokemon with fixed search bar

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,49 @@
 <template>
-	<div id="app" class="app">
-		<img class="logo-pokemon" alt="logo" src="/images/pokemon.png">
-		<div class="search-container">
-			<i 
-				class="ri-search-line search-button"
-				@click="searchPokemon()"></i>
-			<input 
-				type="text" 
-				class="search-bar" 
-				v-model="search" 
-				placeholder="Search"
-				v-on:keyup.enter="searchPokemon()"/>
-			<i 
-				class="ri-close-line search-reset" 
-				@click="searchReset()"></i>
-		</div>
-		<div class="container-list">
-			<PokemonList 
-				ref="pokemonList"
-				:search="searchList"
-			/>
-			<PokemonInfo
-				:name="namePokemon"
-				:url="urlPokemon"
-				:clicked="clickedList"
-			/>
-		</div>
+	<i 
+		class="ri-search-line search-toggle"
+		@click="searchToggle()"
+		:class="{active: isSearchActive}"
+		></i>
+	<div 
+		class="search-container"
+		:class="{active: isSearchActive}"
+		>
+		<i 
+			class="ri-search-line search-button"
+			@click="searchPokemon()"></i>
+		<input 
+			type="text" 
+			class="search-bar" 
+			v-model="search" 
+			placeholder="Search a pokémon"
+			v-on:keyup.enter="searchPokemon()"/>
+		<i 
+			class="ri-close-line search-reset" 
+			@click="searchReset()"></i>
+	</div>
+	<h1>Pokédex</h1>
+	<div class="container-list">
+		<PokemonList 
+			ref="pokemonList"
+			:search="searchList"
+		/>
+		<!-- <PokemonInfo
+			:name="namePokemon"
+			:url="urlPokemon"
+			:clicked="clickedList"
+		/> -->
 	</div>
 </template>
 
 <script>
-import PokemonInfo from "./components/PokemonInfo"
+// import PokemonInfo from "./components/PokemonInfo"
 import PokemonList from "./components/PokemonList"
 import $ from 'jquery'
 
 export default {
 	name: "App",
 	components: {
-		PokemonInfo,
+		// PokemonInfo,
 		PokemonList,
 	},
 	data() {
@@ -46,10 +52,21 @@ export default {
 			urlPokemon: "https://pokeapi.co/api/v2/pokemon/pikachu",
 			search: "",
 			searchList: "",
-			clickedList: false
+			clickedList: false,
+			isSearchActive: false
 		}
 	},
 	methods:{
+		searchToggle(){
+			//Trigger de la recherche
+			//On change la valeur de isSearchActive
+			if(this.isSearchActive == true){
+				this.isSearchActive = false
+			}else{
+				this.isSearchActive = true
+			}
+			
+		},
 		majData(name,url,type){
 			this.namePokemon = name;
 			this.urlPokemon = url;
@@ -88,63 +105,102 @@ export default {
 </script>
 
 <style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Lalezar&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap');
+
 	#app{
-		text-align: center;
+		padding: 30px;
+		&:before{
+			content:'';
+			top: 0;
+			right: 0;
+			position: fixed;
+			display: block;
+			width: 15rem;
+			height: 16.688rem;
+			background: url("./assets/background-pokeball.png");
+			background-size: cover;
+		}
 		*{
 			box-sizing: border-box;
+			font-family: 'Poppins', sans-serif;
 		}
-	}
-	.app {
-		font-family: "Avenir", Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin: 50px 20px;
-	}
-	.logo-pokemon{
-		margin-bottom: 50px;
-	}
-	.nav{
-		display: flex;
-		flex-direction: row;
-		column-gap: 50px;
-		justify-content: center;
-	}
-	.search-container{
-		width: 400px;
-		margin: 0 auto;
-		position: relative;
-		.search-button{
+		.search-toggle{
+			position: fixed;
+			right: 10px;
+			top: 40px;
+			font-size: 30px;
 			cursor: pointer;
-			position: absolute;
-			left: 15px;
-			top: 50%;
-			transform: translateY(-50%);
-		}
-		.search-reset{
-			cursor: pointer;
-			position: absolute;
-			right: 15px;
-			top: 50%;
-			transform: translateY(-50%);
-		}
-		.search-bar{
-			width: 100%;
-			padding: 10px 40px;
-			border: none;
-			border-radius: 20px;
-			border: 2px solid #3264af;
-			transition: all 400ms ease-in;
-			&:focus-visible{
-				outline: none;
+			transition: all 400ms ease-in-out;
+			z-index: 1;
+			padding: 20px 30px;
+			background-color: #f5f5f7;
+			border-radius: 50px;
+			display: flex;
+			z-index: 3;
+			&:before{
+				transition: all 400ms ease-in-out;
 			}
-			&:focus,&:active{
-				outline: none;
-				border: 2px solid #17285e;
+			&:hover{
+				&:before{
+					transform: scale(1.4);
+				}
+			}
+			&.active{
+				&:before{
+					content: "\eb99";
+				}
 			}
 		}
+		.search-container{
+			width: 400px;
+			top: 40px;
+			left: 50%;
+			margin-left: -200px;
+			position: fixed;
+			display: none;
+			z-index: 3;
+			&.active{
+				display: flex;
+			}
+			.search-button{
+				cursor: pointer;
+				position: absolute;
+				left: 15px;
+				top: 50%;
+				transform: translateY(-50%);
+			}
+			.search-reset{
+				cursor: pointer;
+				position: absolute;
+				right: 15px;
+				top: 50%;
+				transform: translateY(-50%);
+			}
+			.search-bar{
+				width: 100%;
+				padding: 10px 40px;
+				border: none;
+				border-radius: 20px;
+				border: 2px solid #f5f5f7;
+				transition: all 400ms ease-in;
+				&:focus-visible{
+					outline: none;
+				}
+				&:focus,&:active{
+					outline: none;
+					border: 2px solid #2b2b2b;
+				}
+			}
+		}
 	}
+	h1{
+		font-family: 'Lalezar', cursive;
+		font-weight: 700;
+		text-align: left;
+		margin: 30px 0px 0px 0px;
+	}
+	
 	.container-list{
 		width: 100%;
         margin: 50px auto;
--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -9,26 +9,35 @@
 			</div>
 			<div v-else>
                 <ul class="pokemon-list">
-                    <li v-for="pokemon in pokemonList" :key="pokemon.name" @click="getInfoPokemon(pokemon.url,pokemon.name)">
-                        <div class="image-container">
-                            <img class="pokemon-img" :src="pokemon.img" :alt="pokemon.name" />
+                    <li 
+                        v-for="pokemon in pokemonList" 
+                        :key="pokemon.id" 
+                        @click="getInfoPokemon(pokemon.url,pokemon.name)"
+                        :class="pokemon.class"
+                        >
+                        <div class="top">
+                            <span class="id">#{{ pokemon.id }}</span>
                         </div>
-                        <span>{{ pokemon.name }}</span>
-                        <i class="ri-arrow-right-s-line"></i>
+                        <div class="bottom">
+                            <div class="left">
+                                <span class="name">{{ pokemon.name }}</span>
+                                <div class="types">
+                                    <span 
+                                        v-for="type in pokemon.types" 
+                                        :key="type.slot"
+                                        >
+                                        {{ type.type.name }}
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="right">
+                                <img class="pokemon-img" :src="pokemon.img" :alt="pokemon.name" />
+                            </div>
+                        </div>
                     </li>
                 </ul>
                 <div v-if="noSearch" class="error">
-                    <Paginate
-                        v-model="page"
-                        :page-count="maxPage"
-                        :page-range="3"
-                        :margin-pages="2"
-                        :click-handler="paginationClick"
-                        :prev-text="'Prev'"
-                        :next-text="'Next'"
-                        :container-class="'pagination'"
-                        :page-class="'page-item'"
-                    />
+
                 </div>
 			</div>
         </div>
@@ -37,13 +46,12 @@
 
 <script>
 import axios from "axios"
-import Paginate from "vuejs-paginate-next"
 
 export default {
 	name: "PokemonList",
 	props: ["search"],
     components: {
-		Paginate
+		
 	},
 	watch: {
         search: function(search) {
@@ -52,27 +60,31 @@ export default {
 	},
 	data() {
 		return {
-            maxPage: "",
-            pokemonByPage: 10,
 			loading: false,
-            url: "https://pokeapi.co/api/v2/pokemon/?limit=10",
+            url: "https://pokeapi.co/api/v2/pokemon/?limit=20",
             urlSearch: "https://pokeapi.co/api/v2/pokemon/?limit=1200",
             pokemonList:[],
             noSearch: true,
 		}
 	},
 	methods: {
-        paginationClick(num){
-            this.loadingList = true
-            let newOffset = (num-1) * this.pokemonByPage
-            let newUrl = "https://pokeapi.co/api/v2/pokemon/?offset="+newOffset+"&limit="+this.pokemonByPage
-            this.getPokemonList(newUrl)
+        //Pour mettre enb forme correctement l'ID
+        //rajoute autant de 0 possible suivant la taille
+        padLeadingZeros(num, size) {
+            var s = num+"";
+            while (s.length < size) s = "0" + s;
+            return s;
         },
-        getPokemonListImg(url,key){
+        getPokemonListInfo(url,key){
             axios.get(url)
             .then(response => {
-                //On vient retourner l'image du pokemon dans l'objet pokemonList
-                this.pokemonList[key].img = response.data.sprites.front_default
+                //On vient retourner les infos voulus du pokemon dans l'objet pokemonList
+                //Pour la class on prend le premier type comme base
+                this.pokemonList[key].id = this.padLeadingZeros(response.data.id,4)
+                this.pokemonList[key].types = response.data.types
+                this.pokemonList[key].class = response.data.types[0].type.name
+                //On regarde si il n'y a pas d'image
+                this.pokemonList[key].img = response.data.sprites.other.dream_world.front_default
                 this.pokemonList[key].url = url
             })
             .catch(error => { 
@@ -101,27 +113,21 @@ export default {
             const self = this
 			this.loading = true
 			this.noSearch = true
+            axios.get(self.url)
+            .then(response => {
+                //On vient mettre la liste à jour
+                this.pokemonList = response.data.results
 
-			setTimeout(()=>{
-				axios.get(self.url)
-				.then(response => {
-                    //On met à jour la pagination avec le nombre de page max
-                    this.maxPage = Math.ceil(response.data.count / this.pokemonByPage) - 1 
-                    
-                    //On vient mettre la liste à jour
-					this.pokemonList = response.data.results
-
-                    //on vient mettre à jour l'img pour chaque objet pokemon
-                    //en appelant via l'url de chaque pokemon
-                    this.pokemonList.map(function(value, key) {
-                        self.getPokemonListImg(value.url,key)
-                    })
-				})
-				.catch(error => { 
-					console.log(error)
-				})
-				.finally(() => (this.loading = false))
-			},500)
+                //on vient mettre à jour l'img pour chaque objet pokemon
+                //en appelant via l'url de chaque pokemon
+                this.pokemonList.map(function(value, key) {
+                    self.getPokemonListInfo(value.url,key)
+                })
+            })
+            .catch(error => { 
+                console.log(error)
+            })
+            .finally(() => (this.loading = false))
 		},
         getInfoPokemon(url,name){
             this.$parent.majData(name,url,'click')
@@ -131,25 +137,23 @@ export default {
             this.noSearch = false
             this.loading = true
 
-            setTimeout(()=>{
-                axios.get(this.urlSearch)
-                .then(response => {
-                    //On vient mettre la liste à jour
-                    this.pokemonList = response.data.results.filter(pokemonList =>
-                        pokemonList.name.toLowerCase().includes(search.toLowerCase())
-                    );
+            axios.get(this.urlSearch)
+            .then(response => {
+                //On vient mettre la liste à jour
+                this.pokemonList = response.data.results.filter(pokemonList =>
+                    pokemonList.name.toLowerCase().includes(search.toLowerCase())
+                );
 
-                    //on vient mettre à jour l'img pour chaque objet pokemon
-                    //en appelant via l'url de chaque pokemon
-                    this.pokemonList.map(function(value, key) {
-                        self.getPokemonListImg(value.url,key)
-                    })
+                //on vient mettre à jour l'img pour chaque objet pokemon
+                //en appelant via l'url de chaque pokemon
+                this.pokemonList.map(function(value, key) {
+                    self.getPokemonListImg(value.url,key)
                 })
-                .catch(error => { 
-                    console.log(error)
-                })
-                .finally(() => (this.loading = false))
-            },500)
+            })
+            .catch(error => { 
+                console.log(error)
+            })
+            .finally(() => (this.loading = false))
         }
 	},
 	mounted () {
@@ -164,58 +168,89 @@ export default {
     @import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
 
     .pokemon-item{
-        // max-width: 75%;
-        // margin: 50px auto;
         width: 100%;
-        background: white;
-        border-radius: 20px;
-        padding: 50px;
         display: flex;
         flex-direction: column;
         row-gap: 20px;
+        z-index: 1;
         transition: all 400ms ease-in-out;
         ul.pokemon-list{
             list-style: none;
             margin: 0;
             padding: 0;
-            display: flex;
-            flex-direction: column;
+            display: grid;
+            grid-template-columns: repeat(3,1fr);
+            column-gap: 20px;
+            row-gap: 20px;
             margin-bottom: 20px;
             li{
                 display: flex;
-                flex-direction: row;
+                flex-direction: column;
                 align-items: center;
-                border-bottom: 1px solid rgb(134, 134, 134);
                 cursor: pointer;
+                border-radius: 30px;
+                background-image: url("../assets/background-pokemon.png");
+                background-position: right bottom;
+                background-repeat: no-repeat;
+                background-size: 60%;
                 padding: 20px;
-                .image-container{
-                    margin-right: 20px;
-                    img{
-                        transition: all 400ms ease-in-out;
+                .top{
+                    display: flex;
+                    width: 100%;
+                    margin-bottom: 30px;
+                    .id{
                         width: 100%;
+                        font-size: 30px;
+                        font-weight: 700;
+                        text-align: right;
+                        opacity: 0.15;
+                        color: white;
+                        font-family: 'Lalezar', cursive;
                     }
                 }
-                span{
-                    text-transform: capitalize;
-                    font-size: 18px;
-                    transition: all 400ms ease-in-out;
-                }
-                i{
-                    transition: all 400ms ease-in-out;
-                    font-size: 18px;
-                }
-                &:hover{
-                    .image-container{
-                        img{
-                            transform: scale(1.1);
+                .bottom{
+                    display: flex;
+                    flex-direction: row;
+                    width: 100%;
+                    .left{
+                        width: 50%;
+                        display: flex;
+                        flex-direction: column;
+                        .name{
+                            text-transform: capitalize;
+                            color: white;
+                            font-size: 30px;
+                            font-weight: bold;
+                            margin-bottom: 30px;
+                        }
+                        .types{
+                            display: flex;
+                            flex-direction: column;
+                            row-gap: 15px;
+                            margin-bottom: 30px;
+                            span{
+                                color:white;
+                                text-transform: capitalize;
+                                background-color: rgba(255, 255, 255, 0.2);
+                                border-radius: 30px;
+                                padding: 10px 20px;
+                                display: inline-block;
+                                width: fit-content;
+                            }
                         }
                     }
-                    span{
-                        transform: translateX(30px);
+                    .right{
+                        width: 50%;
+                        display: flex;
+                        img{
+                            transition: all 400ms ease-in-out;
+                            width: 100%;
+                            max-height: 12.5rem;
+                        }
                     }
-                    i{
-                        transform: translateX(30px);
-                    }
+                }
+                &:hover{
+
                 }
             }
         }
@@ -228,27 +263,79 @@ export default {
 		}
 	}
 
+    //////////////////////////////
+	/////   Color List Type //////
+	//////////////////////////////
+    .normal{
+        background-color: #A8A77A;
+    }
+    .fighting{
+        background-color: #C22E28;
+    }
+    .flying{
+        background-color: #A98FF3;
+    }
+    .poison{
+        background-color: #A33EA1;
+    }
+    .ground{
+        background-color: #E2BF65;
+    }
+    .rock{
+        background-color: #B6A136;
+    }
+    .bug{
+        background-color: #A6B91A;
+    }
+    .ghost{
+        background-color: #735797;
+    }
+    .steel{
+        background-color: #B7B7CE;
+    }
+    .fire{
+        background-color: #EE8130;
+    }
+    .water{
+        background-color: #6390F0;
+    }
+    .grass{
+        background-color: #7AC74C;
+    }
+    .electric{
+        background-color: #F7D02C;
+    }
+    .psychic{
+        background-color: #F95587;
+    }
+    .ice{
+        background-color: #96D9D6;
+    }
+    .dragon{
+        background-color: #6F35FC;
+    }
+    .dark{
+        background-color: #705746;
+    }
+    .fairy{
+        background-color: #D685AD;
+    }
+    .unknown{
+        background-color: #9ea382;
+    }
+    .shadow{
+        background-color: #080c46;
+    }
+
     ///////////////////////////
 	/////   MediaQueries //////
 	///////////////////////////
 	@media screen and (max-width: 1100px) {
-        .pokemon-item{
-			max-width: 100%;
-			margin: 20px 0;
-			padding: 30px;
-        }
-        .pagination {
-            overflow: scroll;
-        }
+        
     }
     
     @media screen and (max-width: 500px) {
-		.pokemon-item{
-			padding: 15px;
-        }
-        .pagination {
-            flex-direction: column;
-        }
+		
     }
         
 
